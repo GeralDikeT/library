759195cc87218843d941d0771b78fee5d2a99b3a
ferrout <dagelishwiligregori@gmail.com>
2025-04-09 06:32:20 +0200

    Добавьте файлы проекта.

---
+++ Library/DataBase/DatabaseHelper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using Microsoft.Data.Sqlite;
+
+namespace Library.DateBase
+{
+    public class DatabaseHelper
+    {
+        private const string DbFile = "library.db";
+
+        public DatabaseHelper()
+        {
+            if (!File.Exists(DbFile))
+            {
+                SQLiteConnection.CreateFile(DbFile);    
+                CreateTable();
+            }
+        }
+
+        private SQLiteConnection GetConnection() =>
+            new SQLiteConnection($"Data Source={DbFile};Version=3;");
+
+        private void CreateTable()
+        {
+            using var conn = GetConnection();
+            conn.Open();
+            var cmd = new SQLiteCommand(@"
+                CREATE TABLE IF NOT EXISTS Books (
+                    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    Title TEXT,
+                    Author TEXT,
+                    Year INTEGER,
+                    Description TEXT
+                )", conn);
+            cmd.ExecuteNonQuery();
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            var books = new List<Book>();
+            using var conn = GetConnection();
+            conn.Open();
+            var cmd = new SQLiteCommand("SELECT * FROM Books", conn);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                books.Add(new Book
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Title = reader["Title"].ToString(),
+                    Author = reader["Author"].ToString(),
+                    Year = Convert.ToInt32(reader["Year"]),
+                    Description = reader["Description"].ToString()
+                });
+            }
+            return books;
+        }
+
+        public int AddBook(Book book)
+        {
+            using var conn = GetConnection();
+            conn.Open();
+            var cmd = new SQLiteCommand("INSERT INTO Books (Title, Author, Year, Description) VALUES (@Title, @Author, @Year, @Description); SELECT last_insert_rowid();", conn);
+            cmd.Parameters.AddWithValue("@Title", book.Title);
+            cmd.Parameters.AddWithValue("@Author", book.Author);
+            cmd.Parameters.AddWithValue("@Year", book.Year);
+            cmd.Parameters.AddWithValue("@Description", book.Description);
+            return Convert.ToInt32(cmd.ExecuteScalar());
+        }
+
+        public void UpdateBook(Book book)
+        {
+            using var conn = GetConnection();
+            conn.Open();
+            var cmd = new SQLiteCommand("UPDATE Books SET Title = @Title, Author = @Author, Year = @Year, Description = @Description WHERE Id = @Id", conn);
+            cmd.Parameters.AddWithValue("@Id", book.Id);
+            cmd.Parameters.AddWithValue("@Title", book.Title);
+            cmd.Parameters.AddWithValue("@Author", book.Author);
+            cmd.Parameters.AddWithValue("@Year", book.Year);
+            cmd.Parameters.AddWithValue("@Description", book.Description);
+            cmd.ExecuteNonQuery();
+        }
+
+        public void DeleteBook(int id)
+        {
+            using var conn = GetConnection();
+            conn.Open();
+            var cmd = new SQLiteCommand("DELETE FROM Books WHERE Id = @Id", conn);
+            cmd.Parameters.AddWithValue("@Id", id);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
---
+++ Library/MainWindow.xaml.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using Library.DateBase;
+
+namespace Library
+{
+    public partial class MainWindow : Window
+    {
+        int i = 0;
+        private DatabaseHelper db;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button_Delete.IsEnabled = false;
+            Button_Edit.IsEnabled = false;
+            db = new DatabaseHelper();
+            LoadBooksFromDatabase();
+        }
+
+        private void Button_AddNote_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBox_Author.Text != "" &&
+                TextBox_Description.Text != "" &&
+                TextBox_NameOfBoock.Text != "" &&
+                TextBox_PublishedYear.Text != "")
+            {
+                if (Button_AddNote.Content.ToString() == "Edit")
+                {
+                    BoockInfo selectedBoockInfo = (BoockInfo)ListBox_Boock.SelectedItem;
+
+                    selectedBoockInfo.TextBlock_NameOfBoock.Text = TextBox_NameOfBoock.Text;
+                    selectedBoockInfo.TextBlock_AuthorOfBoock.Text = TextBox_Author.Text;
+                    selectedBoockInfo.TextBlock_PublishDate.Text = TextBox_PublishedYear.Text;
+                    selectedBoockInfo.TextBlock_Description.Text = TextBox_Description.Text;
+
+                    db.UpdateBook(new Book
+                    {
+                        Id = Convert.ToInt32(selectedBoockInfo.TextBlock_NumberOfBooock.Text.Split(':')[1].Trim()),
+                        Title = TextBox_NameOfBoock.Text,
+                        Author = TextBox_Author.Text,
+                        Year = Convert.ToInt32(TextBox_PublishedYear.Text),
+                        Description = TextBox_Description.Text
+                    });
+
+                    Button_AddNote.Content = "Add Book";
+                }
+                else
+                {
+                    Book book = new Book
+                    {
+                        Title = TextBox_NameOfBoock.Text,
+                        Author = TextBox_Author.Text,
+                        Year = Convert.ToInt32(TextBox_PublishedYear.Text),
+                        Description = TextBox_Description.Text
+                    };
+
+                    var bookId = db.AddBook(book);
+
+                    BoockInfo newBoockInfo = new BoockInfo
+                    {
+                        TextBlock_NameOfBoock = { Text = book.Title },
+                        TextBlock_AuthorOfBoock = { Text = book.Author },
+                        TextBlock_PublishDate = { Text = book.Year.ToString() },
+                        TextBlock_Description = { Text = book.Description },
+                        TextBlock_NumberOfBooock = { Text = $"Number Of Book: {ListBox_Boock.Items.Count + 1}" }
+                    };
+
+                    ListBox_Boock.Items.Add(newBoockInfo);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Error, Please, Enter all Fields!");
+            }
+
+            TextBox_Author.Text = "";
+            TextBox_Description.Text = "";
+            TextBox_NameOfBoock.Text = "";
+            TextBox_PublishedYear.Text = "";
+        }
+
+
+
+        private void Button_Delete_Click(object sender, RoutedEventArgs e)
+        {
+            BoockInfo selectedBoockInfo = (BoockInfo)ListBox_Boock.SelectedItem;
+            int bookId = Convert.ToInt32(selectedBoockInfo.TextBlock_NumberOfBooock.Text.Split(':')[1].Trim());
+            db.DeleteBook(bookId);
+            ListBox_Boock.Items.Remove(selectedBoockInfo);
+            ReassignBookIds();
+        }
+
+        private void ListBox_Boock_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Button_Delete.IsEnabled = true;
+            Button_Edit.IsEnabled = true;
+        }
+
+        private void Button_Edit_Click(object sender, RoutedEventArgs e)
+        {
+            ListBox_Boock.IsEnabled = false;
+            Button_AddNote.Content = "Edit";
+
+            BoockInfo selectedBoockInfo = (BoockInfo)ListBox_Boock.SelectedItem;
+            TextBox_NameOfBoock.Text = selectedBoockInfo.TextBlock_NameOfBoock.Text;
+            TextBox_Author.Text = selectedBoockInfo.TextBlock_AuthorOfBoock.Text;
+            TextBox_PublishedYear.Text = selectedBoockInfo.TextBlock_PublishDate.Text;
+            TextBox_Description.Text = selectedBoockInfo.TextBlock_Description.Text;
+        }
+
+        private void ReassignBookIds()
+        {
+            int id = 1;
+            foreach (BoockInfo bookInfo in ListBox_Boock.Items)
+            {
+                bookInfo.TextBlock_NumberOfBooock.Text = $"Number Of Book: {id++}";
+            }
+        }
+        private void LoadBooksFromDatabase()
+        {
+            var books = db.GetAllBooks();
+            int id = 1;
+            foreach (var book in books)
+            {
+                BoockInfo newBoockInfo = new BoockInfo
+                {
+                    TextBlock_NameOfBoock = { Text = book.Title },
+                    TextBlock_AuthorOfBoock = { Text = book.Author },
+                    TextBlock_PublishDate = { Text = book.Year.ToString() },
+                    TextBlock_Description = { Text = book.Description },
+                    TextBlock_NumberOfBooock = { Text = $"Number Of Book: {id++}" }
+                };
+
+                ListBox_Boock.Items.Add(newBoockInfo);
+            }
+        }
+
+    }
+}
